94af307e77f2e8a2a603ed69eafe831df09c6de6
Cody Samuels <twowoodenspoons@gmail.com>
2020-10-23 18:22:51 -0700

    added all the class methods

--- assets/js/utilities/API.js
+++ assets/js/utilities/API.js
@@ -22,16 +22,39 @@ const API = {
     // Logs the user out.
     logout: () => axios.get(`${baseURL}/api/users/logout/`),
 
-    // Registers the user. Expects an object with email, password, firstName, lastName, address, city, state, zip, phoneNumber, and confirmedEmail
+    // Registers the user. Expects an object with the keys of email, password, firstName, lastName, address, city, state, zip, and, phoneNumber.
     register: (userData) => axios.post(`${baseURL}/api/users/register`, userData),
 
     // Updates a user. Requires two items. The first is the user's ID. This is stored in the cookie. The second is an object with all the parameters you'd like to update.
     updateUser: (id, userData) => axios.put(`${baseURL}/api/users/update/${id}`, userData),
 
     // Deletes a user's profile. I doubt this will even get used.
-    deleteUser: (id) => axios.get(`${baseURL}/api/users/delete/${id}`),
+    deleteUser: (id) => axios.delete(`${baseURL}/api/users/delete/${id}`),
 
     // CLASS METHODS
+    // ======================================================
+    // Retreives all the classes and their data.
+    getAllClasses: () => axios.get(`${baseURL}/api/classes/getAll/`),
+
+    // Retreives a single classes info.
+    getSingleClass: (id) => axios.get(`${baseURL}/api/classes/${id}`),
+
+    // Registers a class. Expects an object with the keys of adminId(optional), name, price, inventory(How many seats left), description, isActive(Whether the class is still going), time, and gradeRange.
+    register: (classData) => axios.post(`${baseURL}/api/classes/create`, classData),
+
+    // Adds a student to any class' roster. Expects an object with the keys of itemId(the class you'd like to add a student to), and userId(the user you want to add).
+    addStudent: (classData) => axios.post(`${baseURL}/api/classes/create`, classData),
+
+    // Updates a classes information. Requires two items. The first is the class' ID. The second is an object with all the parameters you'd like to update.
+    updateClass: (id, classData) => axios.put(`${baseURL}/api/classes/update/${id}`, classData),
+
+    // Deletes a class. Alternatively, considering marking a class inactive.
+    deleteClass: (id) => axios.delete(`${baseURL}/api/classes/delete/${id}`),
+
+    // Deletes a user's profile. I doubt this will even get used.
+    removeStudent: (itemId, userId) => axios.delete(`${baseURL}/api/classes/removeStudent/${itemId}/${userId}`),
+
+        // SHOPPING METHODS
     // ======================================================
 }
 
